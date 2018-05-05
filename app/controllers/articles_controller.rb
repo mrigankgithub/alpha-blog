b7ef3c6e19c445ab89b47862edcf3ac7e261d34e
mrigank <kittu3217@gmail.com>
2018-05-05 20:58:23 +0000

    Update current_user to a article creater

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -24,10 +24,10 @@ class ArticlesController < ApplicationController
   
       def create
         
-        debugger
+        
 
           @article = Article.new(article_params)
-          @article.user = User.first
+          @article.user = current_user
 
       if @article.save
 
